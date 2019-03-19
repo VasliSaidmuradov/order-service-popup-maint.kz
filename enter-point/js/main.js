1d15ba98e624fb9946519800b34ab0942f03f8c2
Vasli Saidmuradov <vasli.said@gmail.com>
2019-03-19 17:46:39 +0600

    Some JS to open and close windows > start - wellcome window - step 0

--- enter-point/js/main.js
+++ enter-point/js/main.js
@@ -1,24 +1,43 @@
+    // GET BLOCKS
     // Get the modal
     var modal = document.getElementById("modal");
-    var bg_opacity = document.getElementById("bgOpacity");
+    var bgOpacity = document.getElementById("bgOpacity");
 
+    //Wellcome box
+    var wellcomeBox = document.getElementById("wellcomeBox");
+    var congratulateBox = document.getElementById("congratulateBox");
+    var levelBox = document.getElementById("levelBox");
+
+
+    // GET BUTTONS
     // Get the button that opens the modal
-    var btn = document.getElementById("btnStart");
+    var btnStartModal = document.getElementById("btnOpenModal");
+    // Get the button that starts game (step 1)
+    var btnStartGame = document.getElementById("btnStartGame");
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    // Get the button that closes the modal
+    var btnClose = document.getElementsByClassName("close")[0];
 
-    // When the user clicks the button, open the modal 
-    btn.onclick = function () {
+
+    // When the user clicks the button, open the modal and wellcome message
+    btnStartModal.onclick = function () {
       modal.style.display = "block";
-      bg_opacity.style.display = "block";
-
+      bgOpacity.style.display = "block";
+      wellcomeBox.style.display = "block"
     }
-
+    btnStartGame.onclick = function () {
+      // modal.style.display = "block";
+      // bgOpacity.style.display = "block";
+      wellcomeBox.style.display = "none"
+      // document.getElementById("levelBox").style.display = "block";
+      levelBox.style.display = "block"
+    }
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
+    btnClose.onclick = function () {
       modal.style.display = "none";
-      bg_opacity.style.display = "none";
+      bgOpacity.style.display = "none";
+      wellcomeBox.style.display = "none"
+      levelBox.style.display = "none";
     }
 
     // When the user clicks anywhere outside of the modal, close it
@@ -27,6 +46,6 @@
         modal.style.display = "none";
       }
       if (event.target == bg_opacity) {
-        bg_opacity.style.display = "none";
+        bgOpacity.style.display = "none";
       }
     }
